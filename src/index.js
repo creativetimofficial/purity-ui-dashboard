95fec49738046b3fbf904199a8cf57acc7f5dd4e
VladMihalache <59883066+VladMihalache@users.noreply.github.com>
2021-08-23 14:12:50 +0300

    Router Change

--- src/index.js
+++ src/index.js
@@ -17,17 +17,23 @@
 */
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import {
+  BrowserRouter,
+  Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
 import AuthLayout from "layouts/Auth.js";
 import AdminLayout from "layouts/Admin.js";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={`${process.env.PUBLIC_URL}/`}>
     <Switch>
-      <Route path="/auth" component={AuthLayout} />
-      <Route path="/admin" component={AdminLayout} />
-      <Redirect from="/" to="/admin/dashboardchakra" />
+      <Route path={`/auth`} component={AuthLayout} />
+      <Route path={`/admin`} component={AdminLayout} />
+      <Redirect from="/" to={`/admin/dashboard`} />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
