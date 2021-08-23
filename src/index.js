a165bbfd04f29c0203e8bb03203d3a4cabbab066
VladMihalache <59883066+VladMihalache@users.noreply.github.com>
2021-08-23 15:46:17 +0300

    hash router

--- src/index.js
+++ src/index.js
@@ -17,18 +17,18 @@
 */
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import AuthLayout from "layouts/Auth.js";
 import AdminLayout from "layouts/Admin.js";
 
 ReactDOM.render(
-  <BrowserRouter basename={`${process.env.PUBLIC_URL}/`}>
+  <HashRouter basename={`${process.env.PUBLIC_URL}/`}>
     <Switch>
       <Route path={`/auth`} component={AuthLayout} />
       <Route path={`/admin`} component={AdminLayout} />
-      <Redirect from={`/`} to={`/admin/dashboard`} />
+      <Redirect from={`/`} to="/admin/dashboard" />
     </Switch>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
