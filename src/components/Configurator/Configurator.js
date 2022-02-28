45cbc01ff55faf7be7ab234992eb17add39e0edf
Mike Cunneen <mike@appworkshop.net>
2022-02-28 13:27:46 +0800

    Fix issue: 'Invalid prop `isOpen` of type `boolean` supplied to `Configurator`, expected `function`'

--- src/components/Configurator/Configurator.js
+++ src/components/Configurator/Configurator.js
@@ -231,7 +231,7 @@ export default function Configurator(props) {
 }
 Configurator.propTypes = {
   secondary: PropTypes.bool,
-  isOpen: PropTypes.func,
+  isOpen: PropTypes.bool,
   onClose: PropTypes.func,
   fixed: PropTypes.bool,
 };
